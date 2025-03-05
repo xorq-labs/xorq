b444a40c98ab50e24f44791cda7644763d9ca892
Daniel Mesejo <mesejoleon@gmail.com>
2025-03-05 20:38:47 +0100

    feat: add run cli command (#581)
    * feat: add run cli command
    
    * chore: add output format
    
    * chore: change target_dir to builds_dir
    
    * chore: change run arguments and functionality
    
    * chore: handle wildcard case

--- python/xorq/tests/test_cli.py
+++ python/xorq/tests/test_cli.py
@@ -1,9 +1,10 @@
+import re
 import sys
 from pathlib import Path
 
 import pytest
 
-from xorq.cli import main
+from xorq.cli import build_command, main
 
 
 def test_build_command(monkeypatch, tmp_path, capsys):
@@ -86,6 +87,63 @@ def test_build_command_on_notebook(monkeypatch, tmp_path, capsys):
     assert "Building expr" in captured.out
 
     assert builds_dir.exists()
+
+
+def test_run_command_default(monkeypatch, tmp_path, capsys):
+    target_dir = tmp_path / "build"
+    script_path = Path(__file__).absolute().parent / "fixtures" / "pipeline.py"
+
+    build_command(str(script_path), ["expr"], builds_dir=str(target_dir))
+    capture = capsys.readouterr()
+
+    if match := re.search(f"{target_dir}/([0-9a-f]+)", str(capture.out)):
+        expression_path = match.group()
+        test_args = [
+            "xorq",
+            "run",
+            expression_path,
+        ]
+        monkeypatch.setattr(sys, "argv", test_args)
+
+        try:
+            main()
+        except SystemExit:
+            pass
+    else:
+        raise AssertionError("No expression hash")
+
+
+@pytest.mark.parametrize("output_format", ["csv", "json", "parquet"])
+def test_run_command(monkeypatch, tmp_path, capsys, output_format):
+    target_dir = tmp_path / "build"
+    script_path = Path(__file__).absolute().parent / "fixtures" / "pipeline.py"
+
+    build_command(str(script_path), ["expr"], builds_dir=str(target_dir))
+    capture = capsys.readouterr()
+
+    if match := re.search(f"{target_dir}/([0-9a-f]+)", str(capture.out)):
+        output_path = tmp_path / f"test.{output_format}"
+        expression_path = match.group()
+        test_args = [
+            "xorq",
+            "run",
+            expression_path,
+            "--output-path",
+            str(output_path),
+            "--format",
+            output_format,
+        ]
+        monkeypatch.setattr(sys, "argv", test_args)
+
+        try:
+            main()
+        except SystemExit:
+            pass
+        assert output_path.exists()
+        assert output_path.stat().st_size > 0
+
+    else:
+        raise AssertionError("No expression hash")
 
 
 def test_build_command_not_implemented(monkeypatch, capsys):
