97ae37b91f032d969a236c0dd83b41421a871d9f
dlovell <dlovell@gmail.com>
2025-03-11 17:26:50 -0400

    fix(caching): special case for postgres in case of complex types
    dt.Array(dt.float64) causes failures

---
+++ python/xorq/backends/postgres/tests/test_cache.py
@@ -0,0 +1,27 @@
+import pytest
+
+import xorq as xo
+from xorq.caching import SourceStorage
+
+
+@pytest.mark.parametrize(
+    "example",
+    (
+        "diamonds",
+        "hn-data-small.parquet",
+    ),
+)
+def test_source_caching(example, pg):
+    con = xo.connect()
+    name = "diamonds"
+    example = xo.examples.get_table_from_name(name, con)
+    expr = example.cache(storage=SourceStorage(pg))
+    assert not expr.ls.exists()
+    actual = expr.execute()
+    expected = example.execute()
+    cached = pg.table(expr.ls.get_key()).execute()
+    assert actual.equals(expected)
+    assert actual.equals(cached)
+    assert expr.ls.exists()
+    pg.drop_table(expr.ls.get_key())
+    assert not expr.ls.exists()
