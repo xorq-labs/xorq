6fd6e1b0140a633c20d8b18f2debec4eeb2069ca
Hussain Sultan <hussainz@gmail.com>
2025-03-16 15:15:37 -0400

    feat: add dummy exchanger

--- examples/flight_dummy_exchanger.py
+++ examples/flight_dummy_exchanger.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import toolz
 
 import xorq as xo
 from xorq.flight import FlightServer, FlightUrl
+from xorq.flight.action import AddExchangeAction
 from xorq.flight.exchanger import make_udxf
 
 
@@ -18,11 +18,18 @@ schema_out = xo.schema({"row_count": "int64"})
 
 dummy_udxf = make_udxf(dummy, schema_in.to_pyarrow(), schema_out.to_pyarrow())
 
-flight_server = FlightServer(FlightUrl(port=flight_port), exchangers=[dummy_udxf])
+flight_server = FlightServer(FlightUrl(port=flight_port))
 flight_server.serve()
 
 client = flight_server.client
+client.do_action(AddExchangeAction.name, dummy_udxf, options=client._options)
 
-do_exchange = toolz.curry(client.do_exchange, dummy_udxf.command)
 
-do_exchange(xo.memtable({"dummy": [0]}, schema=schema_in).to_pyarrow_batches())
+def dummy_do_exchange(input_data):
+    input_table = xo.memtable(input_data, schema=schema_in)
+    rbr_in = input_table.to_pyarrow_batches()
+    _, rbr_out = client.do_exchange(dummy_udxf.command, rbr_in)
+    return rbr_out.read_pandas()
+
+
+dummy_do_exchange(xo.memtable({"dummy": [0]}, schema=schema_in))
