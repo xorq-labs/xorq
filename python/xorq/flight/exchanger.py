d4f72814e9979d373913989c56ca830c6d3d2ca4
dlovell <dlovell@gmail.com>
2025-03-16 18:13:20 -0400

    fix(flight): sanitize table names

--- python/xorq/flight/exchanger.py
+++ python/xorq/flight/exchanger.py
@@ -444,17 +444,33 @@ class PandasUDFExchanger(AbstractExchanger):
 
 class UnboundExprExchanger(AbstractExchanger):
     def __init__(self, unbound_expr, make_connection=xo.connect):
-        self.unbound_expr = unbound_expr
+        if unbound_expr.op().find(ops.DatabaseTable):
+            raise ValueError("unbound_expr must be unbound")
+        self.unbound_expr = self.set_one_unbound_name(unbound_expr)
         self.make_connection = make_connection
-        self._schema_in_required = (
-            self.unbound_expr.op()
-            .find(ops.UnboundTable)[0]
-            .to_expr()
-            .schema()
-            .to_pyarrow()
-        )
+        self._schema_in_required = self.get_one_unbound(
+            self.unbound_expr
+        ).schema.to_pyarrow()
         self._schema_in_condition = toolz.curried.operator.eq(self._schema_in_required)
         self._schema_out = self.unbound_expr.schema().to_pyarrow()
+
+    @staticmethod
+    def get_one_unbound(expr):
+        (unbound, *rest) = expr.op().find(ops.UnboundTable)
+        if rest:
+            raise ValueError("expr must only have one unbound table")
+        return unbound
+
+    @staticmethod
+    def set_one_unbound_name(expr, name="fixed-name"):
+        def set_name(op, kwargs):
+            if isinstance(op, ops.UnboundTable):
+                op = op.copy(name=name)
+            if kwargs:
+                op = op.__recreate__(kwargs)
+            return op
+
+        return expr.op().replace(set_name).to_expr()
 
     @property
     def op_hash(self):
