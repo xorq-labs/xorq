f1070a6d34d9aff9ee8af7c66de31681fe90e630
Daniel Mesejo <mesejoleon@gmail.com>
2025-03-25 14:44:26 +0100

    chore: unbind_socket

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -89,7 +89,8 @@ sentiment_udxf = make_udxf(
 
 # Start the Flight server with our exchanger
 flight_port = 8815
-flight_server = FlightServer(FlightUrl(port=flight_port), exchangers=[sentiment_udxf])
+flight_url = FlightUrl(port=flight_port)
+flight_server = FlightServer(flight_url, exchangers=[sentiment_udxf])
 flight_server.serve()
 
 # Get a client to test the server
@@ -109,6 +110,7 @@ def test_flight_service():
 print("Testing Flight service...")
 test_flight_service()
 flight_server.close()
+flight_url.unbind_socket()
 
 """
 Next Steps: use the cli to build and see how things look like:
