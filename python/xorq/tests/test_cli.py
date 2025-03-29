8f415f5fb24290c5b32a046ac84a5068e3c86e77
Dan Lovell <dlovell@gmail.com>
2025-03-29 10:24:53 -0400

    feat(cli): add default value to expr_name (#748)

--- python/xorq/tests/test_cli.py
+++ python/xorq/tests/test_cli.py
@@ -93,7 +93,7 @@ def test_run_command_default(monkeypatch, tmp_path, capsys):
     target_dir = tmp_path / "build"
     script_path = Path(__file__).absolute().parent / "fixtures" / "pipeline.py"
 
-    build_command(str(script_path), ["expr"], builds_dir=str(target_dir))
+    build_command(str(script_path), "expr", builds_dir=str(target_dir))
     capture = capsys.readouterr()
 
     if match := re.search(f"{target_dir}/([0-9a-f]+)", str(capture.out)):
@@ -118,7 +118,7 @@ def test_run_command(monkeypatch, tmp_path, capsys, output_format):
     target_dir = tmp_path / "build"
     script_path = Path(__file__).absolute().parent / "fixtures" / "pipeline.py"
 
-    build_command(str(script_path), ["expr"], builds_dir=str(target_dir))
+    build_command(str(script_path), "expr", builds_dir=str(target_dir))
     capture = capsys.readouterr()
 
     if match := re.search(f"{target_dir}/([0-9a-f]+)", str(capture.out)):
@@ -144,29 +144,6 @@ def test_run_command(monkeypatch, tmp_path, capsys, output_format):
 
     else:
         raise AssertionError("No expression hash")
-
-
-def test_build_command_not_implemented(monkeypatch, capsys):
-    script_path = Path(__file__).absolute().parent / "fixtures" / "pipeline.py"
-
-    test_args = [
-        "xorq",
-        "build",
-        str(script_path),
-        "--builds-dir",
-        str(Path.cwd()),
-    ]
-    monkeypatch.setattr(sys, "argv", test_args)
-
-    # Run the CLI (with try/except to prevent SystemExit)
-    try:
-        main()
-    except SystemExit:
-        pass
-
-    # Check output
-    captured = capsys.readouterr()
-    assert "Expected one, and only one expression" in captured.err
 
 
 @pytest.mark.parametrize(
