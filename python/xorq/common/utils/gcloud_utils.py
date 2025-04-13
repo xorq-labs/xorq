17c6a40c216bd291c95a6ab89a6088a5a72b3163
Dan Lovell <dlovell@gmail.com>
2025-04-13 07:24:09 -0400

    ref(gcloud_utils): use gcs built in uri ctor (#839)

--- python/xorq/common/utils/gcloud_utils.py
+++ python/xorq/common/utils/gcloud_utils.py
@@ -1,34 +1,19 @@
-from pathlib import Path
-
 from google.cloud import storage
 
 
-def get_file_metadata(file_path, bucket_name=None):
-    # FIXME allow for arbitrary credentials
-    client = storage.Client.create_anonymous_client()
-
-    if bucket_name is None:  # assume the file_path contains the bucket_name
-        _, bucket_name, *file_path_parts = Path(file_path).parts
-        file_path = "/".join(file_path_parts)
-
-    # Access the bucket and blob (file)
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(file_path)
-
+def get_file_metadata(uri, client=None):
+    blob = storage.Blob.from_string(uri)
     # Refresh metadata (required for accurate timestamps)
-    blob.reload()
+    blob.reload(client or storage.Client.create_anonymous_client())
 
     # Extract relevant metadata
-    metadata = (
-        (
+    metadata = tuple(
+        (name, getattr(blob, name))
+        for name in (
             "content_type",
-            blob.content_type,
-        ),
-        (
             "updated",
-            blob.updated,
-        ),
-        ("size", blob.size),
+            "size",
+        )
     )
 
     return metadata
