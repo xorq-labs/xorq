270298bbeb29b6f61bdbaada53b11583aa2a761a
dlovell <dlovell@gmail.com>
2025-04-16 19:53:22 -0400

    feat(env): add env_utils.py, readEnv.nix

---
+++ python/xorq/common/utils/env_utils.py
@@ -0,0 +1,88 @@
+import os
+import re
+from pathlib import Path
+
+from attr import (
+    field,
+    frozen,
+)
+from attr.validators import (
+    instance_of,
+    optional,
+)
+
+
+def parse_env_file(env_file):
+    pattern = "(export )?([^=]+)=(.*)"
+    lines = Path(env_file).read_text().split("\n")
+    matches = filter(None, (re.match(pattern, line) for line in lines))
+    dct = {name: value for name, value in (match.groups()[1:] for match in matches)}
+    return dct
+
+
+@frozen
+class EnvConfigable:
+    def get(self, key, default=None):
+        return getattr(self, key, default)
+
+    def __getitem__(self, key, default=None):
+        return getattr(self, key, default)
+
+    @property
+    def varnames(self):
+        return tuple(self.get_defaults())
+
+    @classmethod
+    def get_env_overrides(cls, mapper=str.upper):
+        dct = {
+            name: value
+            for (name, value) in (
+                (attr.name, os.environ.get(mapper(attr.name)))
+                for attr in cls.__attrs_attrs__
+            )
+            if value is not None
+        }
+        return dct
+
+    @classmethod
+    def get_defaults(cls):
+        dct = {attr.name: attr.default for attr in cls.__attrs_attrs__}
+        return dct
+
+    @classmethod
+    def from_env(cls, **kwargs):
+        return cls(**(kwargs | cls.get_env_overrides()))
+
+    @classmethod
+    def from_kwargs(cls, *args, **kwargs):
+        fields = {
+            name: field(
+                default=os.environ.get(name, ""), validator=optional(instance_of(str))
+            )
+            for name in args
+        } | {
+            name: field(
+                default=value,
+                validator=optional(instance_of(str if value is None else type(value))),
+            )
+            for name, value in kwargs.items()
+        }
+        return frozen(
+            type(
+                "EnvConfig",
+                (cls,),
+                fields,
+            )
+        )
+
+    @classmethod
+    def from_env_file(cls, env_file):
+        env_file = Path(env_file).resolve()
+        return cls.from_kwargs(
+            **(
+                parse_env_file(env_file)
+                | {
+                    "env_file": env_file,
+                }
+            )
+        )
---
+++ python/xorq/common/utils/tests/test_env_utils.py
@@ -0,0 +1,59 @@
+import os
+from pathlib import Path
+
+import pytest
+
+from xorq.common.utils.env_utils import EnvConfigable
+
+
+def test_from_kwargs(monkeypatch):
+    monkeypatch.setitem(os.environ, "X", "1")
+    monkeypatch.delitem(os.environ, "SHOULDNT_EXIST", raising=False)
+
+    EnvConfig = EnvConfigable.from_kwargs("X", "SHOULDNT_EXIST")
+    env_config = EnvConfig.from_env()
+    assert env_config.X == "1"
+    assert env_config.SHOULDNT_EXIST == ""
+
+    # env vars take priority
+    env_config = EnvConfig.from_env(X=2)
+    assert env_config.X == "1"
+    assert env_config.SHOULDNT_EXIST == ""
+
+    # X is an env var so will be passed as a string but must be an int
+    EnvConfig = EnvConfigable.from_kwargs("SHOULDNT_EXIST", X=1)
+    with pytest.raises(TypeError):
+        env_config = EnvConfig.from_env()
+
+    # so when using from_env, non-string fields must not exist in the env
+    monkeypatch.delitem(os.environ, "X")
+    EnvConfig = EnvConfigable.from_kwargs("SHOULDNT_EXIST", X=1)
+    env_config = EnvConfig.from_env()
+    assert env_config.X == 1
+    assert env_config.SHOULDNT_EXIST == ""
+
+
+def test_from_env_file(monkeypatch, tmp_path):
+    content = "\n".join(
+        (
+            "export X=2",
+            "Y=3",
+            "a line that doesn't match, perhaps we should raise?",
+        )
+    )
+    env_file = Path(tmp_path).joinpath(".env")
+    env_file.write_text(content)
+    EnvConfig = EnvConfigable.from_env_file(env_file)
+
+    env_config = EnvConfig()
+    assert env_config.X == "2"
+    assert env_config.Y == "3"
+
+    env_config = EnvConfig.from_env()
+    assert env_config.X == "2"
+    assert env_config.Y == "3"
+
+    monkeypatch.setitem(os.environ, "X", "1")
+    env_config = EnvConfig.from_env()
+    assert env_config.X == "1"
+    assert env_config.Y == "3"
