ff3d3b9d56831b7a1a4a6af06e1967c797d4399a
dlovell <dlovell@gmail.com>
2025-04-21 14:49:00 -0400

    feat(env_utils): add clone method

--- python/xorq/common/utils/env_utils.py
+++ python/xorq/common/utils/env_utils.py
@@ -30,6 +30,11 @@ class EnvConfigable:
     def __getitem__(self, key):
         return getattr(self, key)
 
+    def clone(self, **overrides):
+        return type(self)(
+            **({varname: self[varname] for varname in self.varnames} | overrides)
+        )
+
     @property
     def varnames(self):
         return tuple(self.get_defaults())
