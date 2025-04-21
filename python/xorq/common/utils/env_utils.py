c1a35150d93fe6844883e9f8657c3250a65fd79d
dlovell <dlovell@gmail.com>
2025-04-21 15:03:24 -0400

    fix(env_utils): __getitem__: remove default

--- python/xorq/common/utils/env_utils.py
+++ python/xorq/common/utils/env_utils.py
@@ -30,8 +30,8 @@ class EnvConfigable:
     def get(self, key, default=None):
         return getattr(self, key, default)
 
-    def __getitem__(self, key, default=None):
-        return getattr(self, key, default)
+    def __getitem__(self, key):
+        return getattr(self, key)
 
     @property
     def varnames(self):
