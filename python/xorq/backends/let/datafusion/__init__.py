60a7f54f3a30f1bb4ed2d04cf960020bde6da7d4
Daniel Mesejo <mesejoleon@gmail.com>
2025-04-29 10:48:39 +0200

    feat(xorq): enable reading list in read_parquet and read_csv
    closes #868

--- python/xorq/backends/let/datafusion/__init__.py
+++ python/xorq/backends/let/datafusion/__init__.py
@@ -53,7 +53,7 @@ from xorq.vendor.ibis.expr.operations.udf import InputType
 from xorq.vendor.ibis.formats.pyarrow import (
     PyArrowType,
 )
-from xorq.vendor.ibis.util import gen_name, normalize_filename
+from xorq.vendor.ibis.util import gen_name, normalize_filename, normalize_filenames
 
 
 def _compile_pyarrow_udwf(udwf_node):
@@ -576,7 +576,10 @@ class Backend(SQLBackend, CanCreateCatalog, CanCreateDatabase, CanCreateSchema, 
                 self._register_in_memory_table(memtable)
 
     def read_csv(
-        self, path: str | Path, table_name: str | None = None, **kwargs: Any
+        self,
+        path: str | Path | list[str | Path],
+        table_name: str | None = None,
+        **kwargs: Any,
     ) -> ir.Table:
         """Register a CSV file as a table in the current database.
 
@@ -596,9 +599,10 @@ class Backend(SQLBackend, CanCreateCatalog, CanCreateDatabase, CanCreateSchema, 
             The just-registered table
 
         """
-        path = normalize_filename(path)
+
+        path = normalize_filenames(path)
         table_name = table_name or gen_name("read_csv")
-        kwargs.setdefault("file_extension", Path(path).suffix)
+        kwargs.setdefault("file_extension", Path(path[0]).suffix)
 
         storage_options, is_connection_set = make_s3_connection()
         if is_connection_set:
@@ -616,7 +620,10 @@ class Backend(SQLBackend, CanCreateCatalog, CanCreateDatabase, CanCreateSchema, 
         return self.table(table_name)
 
     def read_parquet(
-        self, path: str | Path, table_name: str | None = None, **kwargs: Any
+        self,
+        path: str | Path | list[str | Path],
+        table_name: str | None = None,
+        **kwargs: Any,
     ) -> ir.Table:
         """Register a parquet file as a table in the current database.
 
@@ -636,9 +643,9 @@ class Backend(SQLBackend, CanCreateCatalog, CanCreateDatabase, CanCreateSchema, 
             The just-registered table
 
         """
-        path = normalize_filename(path)
+        path = normalize_filenames(path)
         table_name = table_name or gen_name("read_parquet")
-        kwargs.setdefault("file_extension", Path(path).suffix)
+        kwargs.setdefault("file_extension", Path(path[0]).suffix)
 
         # Our other backends support overwriting views / tables when reregistering
         self.con.deregister_table(table_name)
