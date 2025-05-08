890dc4be32050348ec80adb2e701b4a3010c7ef1
Daniel Mesejo <mesejoleon@gmail.com>
2025-05-08 14:40:17 +0200

    fix: flight server issues (#922)
    Enables `xorq.flight.connect`
    
    closes #919 
    closes #920

---
+++ python/xorq/flight/tests/test_basic.py
@@ -0,0 +1,15 @@
+import xorq as xo
+from xorq.flight import FlightServer
+from xorq.flight.tests.test_server import make_flight_url
+
+
+def test_connect():
+    flight_url = make_flight_url(None)
+
+    with FlightServer(
+        flight_url=flight_url,
+        verify_client=False,
+        connection=xo.duckdb.connect,
+    ) as _:
+        con = xo.flight.connect(host=flight_url.host, port=flight_url.port)
+        assert con is not None
