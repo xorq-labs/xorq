27f0cad170982bdc72d637dbb8b3aaa3a1e5a927
dlovell <dlovell@gmail.com>
2025-06-19 14:48:41 -0400

    feat(nix): add xorq-install-docker

--- nix/commands.nix
+++ nix/commands.nix
@@ -75,6 +75,27 @@ let
     fi
   '';
 
+  xorq-install-docker = pkgs.writeShellScriptBin "xorq-install-docker" ''
+    set -eux
+
+    # https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo ${pkgs.curl}/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "''${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  '';
+
   xorq-docker-compose-up = pkgs.writeShellScriptBin "xorq-docker-compose-up" ''
     set -eux
 
@@ -148,6 +169,7 @@ let
       xorq-newgrp-docker-compose-up
       xorq-git-config-blame-ignore-revs
       xorq-gh-config-set-browser-false
+      xorq-install-docker
       xorq-docker-run-otel-collector xorq-docker-exec-otel-print-initial-config
       xorq-cachix-use xorq-cachix-push
       ;
