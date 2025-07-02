c31a0bde587729b4d80a0cb54005a4c3383f0cb9
Daniel Mesejo <mesejoleon@gmail.com>
2025-07-02 11:58:28 +0200

    chore(test): run slow tests in a different runner

--- python/xorq/tests/test_cli.py
+++ python/xorq/tests/test_cli.py
@@ -169,6 +169,7 @@ def test_run_command_default(tmp_path, fixture_dir):
         raise AssertionError("No expression hash")
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("output_format", ["csv", "json", "parquet"])
 def test_run_command(tmp_path, fixture_dir, output_format):
     target_dir = tmp_path / "build"
@@ -207,6 +208,7 @@ def test_run_command(tmp_path, fixture_dir, output_format):
         raise AssertionError("No expression hash")
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("cache_dir", [None, "cache"])
 def test_serve_command(tmp_path, fixture_dir, cache_dir):
     target_dir = tmp_path / "build"
@@ -312,6 +314,7 @@ def test_build_command_bad_expr_name(tmp_path, fixture_dir, expression, message)
     ("example", "expr_name"),
     build_run_examples_expr_names,
 )
+@pytest.mark.slow
 def test_examples(
     example,
     expr_name,
