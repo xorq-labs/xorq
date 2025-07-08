6b7d56061e0df84a5822d803cd1e0501061f5cda
Daniel Mesejo <mesejoleon@gmail.com>
2025-07-08 18:44:27 +0200

    fix(examples): fetch penguins should work with duckdb
    closes #1113

--- python/xorq/examples/tests/test_examples.py
+++ python/xorq/examples/tests/test_examples.py
@@ -1,7 +1,7 @@
 import pytest
 
 import xorq as xo
-from xorq.examples import Example
+from xorq.examples import Example, get_name_to_suffix
 from xorq.examples.core import whitelist
 
 
@@ -16,3 +16,42 @@ def test_attributes():
 
     with pytest.raises(AttributeError):
         xo.examples.missing
+
+
+@pytest.mark.parametrize(
+    "fetch",
+    [
+        pytest.param(
+            lambda: xo.examples.penguins.fetch(xo.duckdb.connect(), table_name="t"),
+            id="duckdb-name",
+        ),
+        pytest.param(
+            lambda: xo.examples.penguins.fetch(table_name="t"), id="default-name"
+        ),
+        pytest.param(lambda: xo.examples.penguins.fetch(), id="default-no-name"),
+        pytest.param(
+            lambda: xo.examples.penguins.fetch(deferred=False), id="default-no-deferred"
+        ),
+    ],
+)
+def test_fetch_penguins(fetch):
+    t = fetch()
+    expected = t.execute()
+
+    assert not expected.empty
+    assert expected.shape
+
+
+def test_parquet_examples():
+    parquet_examples = tuple(
+        name for name, suffix in get_name_to_suffix().items() if suffix == ".parquet"
+    )
+    assert len(parquet_examples) > 0
+
+    assert not any(
+        getattr(xo.examples, example_name)
+        .fetch(xo.duckdb.connect(), table_name="t")
+        .execute()
+        .empty
+        for example_name in parquet_examples
+    )
