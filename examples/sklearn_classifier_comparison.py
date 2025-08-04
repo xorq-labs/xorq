f26f6d3e1277c22f693e0af42cde04d6fb2d73f7
dlovell <dlovell@gmail.com>
2025-08-04 14:49:13 -0400

    feat: add xorq pipeline equivalents

--- examples/sklearn_classifier_comparison.py
+++ examples/sklearn_classifier_comparison.py
@@ -3,11 +3,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import numpy as np
+import pandas as pd
 from sklearn.datasets import make_circles, make_classification, make_moons
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
+# from sklearn.gaussian_process import GaussianProcessClassifier
+# from sklearn.gaussian_process.kernels import RBF
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -16,6 +17,18 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+
+import xorq as xo
+from xorq.expr.ml import Pipeline
+from xorq.expr.ml.pipeline_lib import (
+    get_predict_return_type,
+    get_target_type,
+    step_typ_to_f,
+)
+
+
+target = "target"
+feature_prefix = "feature_"
 
 
 def make_linearly_separable():
@@ -31,53 +44,68 @@ def make_linearly_separable():
     return (X, y)
 
 
-names = [
-    "Nearest Neighbors",
-    "Linear SVM",
-    "RBF SVM",
-    "Gaussian Process",
-    "Decision Tree",
-    "Random Forest",
-    "Neural Net",
-    "AdaBoost",
-    "Naive Bayes",
-    "QDA",
-]
+def make_exprs(X_train, y_train, X_test, y_test):
+    con = xo.connect()
+    (train, test) = (
+        con.register(
+            pd.DataFrame(X)
+            .rename(columns=(feature_prefix + "{}").format)
+            .assign(**{target: y}),
+            name,
+        )
+        for (X, y, name) in (
+            (X_train, y_train, "train"),
+            (X_test, y_test, "test"),
+        )
+    )
+    features = train.select(xo.selectors.startswith(feature_prefix)).columns
+    return (train, test, features)
 
 
-classifiers = [
-    KNeighborsClassifier(3),
-    SVC(kernel="linear", C=0.025, random_state=42),
-    SVC(gamma=2, C=1, random_state=42),
-    GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
-    DecisionTreeClassifier(max_depth=5, random_state=42),
-    RandomForestClassifier(
+classifiers = {
+    "Nearest Neighbors": KNeighborsClassifier(3),
+    "Linear SVM": SVC(kernel="linear", C=0.025, random_state=42),
+    "RBF SVM": SVC(gamma=2, C=1, random_state=42),
+    # # issue with hashability of RBF
+    # "Gaussian Process": GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
+    "Decision Tree": DecisionTreeClassifier(max_depth=5, random_state=42),
+    "Random Forest": RandomForestClassifier(
         max_depth=5, n_estimators=10, max_features=1, random_state=42
     ),
-    MLPClassifier(alpha=1, max_iter=1000, random_state=42),
-    AdaBoostClassifier(random_state=42),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis(),
-]
+    "Neural Net": MLPClassifier(alpha=1, max_iter=1000, random_state=42),
+    "AdaBoost": AdaBoostClassifier(random_state=42),
+    "Naive Bayes": GaussianNB(),
+    "QDA": QuadraticDiscriminantAnalysis(),
+}
 
 
-datasets = [
-    make_moons(noise=0.3, random_state=0),
-    make_circles(noise=0.2, factor=0.5, random_state=1),
-    make_linearly_separable(),
-]
+for typ in set(clf.__class__ for clf in classifiers.values()).difference(step_typ_to_f):
+    get_predict_return_type.register(typ, get_target_type)
+
+
+datasets = {
+    "moons": make_moons(noise=0.3, random_state=0),
+    "circles": make_circles(noise=0.2, factor=0.5, random_state=1),
+    "linearly separable": make_linearly_separable(),
+}
 
 
 dct = {}
-for ds_cnt, ds in enumerate(datasets):
-    # preprocess dataset, split into training and test part
-    X, y = ds
+for ds_name, (X, y) in datasets.items():
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.4, random_state=42
     )
-    # iterate over classifiers
-    for name, clf in zip(names, classifiers):
-        clf = make_pipeline(StandardScaler(), clf)
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-        dct[(ds_cnt, name)] = {"clf": clf, "score": score}
+    (train, test, features) = make_exprs(X_train, y_train, X_test, y_test)
+    for clf_name, clf in classifiers.items():
+        sklearn_pipeline = make_pipeline(StandardScaler(), clf).fit(X_train, y_train)
+        sklearn_score = sklearn_pipeline.score(X_test, y_test)
+        xorq_pipeline = Pipeline.from_instance(sklearn_pipeline).fit(
+            train, features=features, target=target
+        )
+        xorq_score = xorq_pipeline.score_expr(test)
+        dct[(ds_name, clf_name)] = {
+            "sklearn_pipeline": sklearn_pipeline,
+            "sklearn_score": sklearn_score,
+            "xorq_pipeline": xorq_pipeline,
+            "xorq_score": xorq_score,
+        }
