50ded4742c8c975993569eed076a323c391de238
Daniel Mesejo <mesejoleon@gmail.com>
2025-08-08 15:25:25 +0200

    chore: warn on local path

--- python/xorq/ibis_yaml/translate.py
+++ python/xorq/ibis_yaml/translate.py
@@ -4,6 +4,8 @@ import datetime
 import decimal
 import functools
 import operator
+import warnings
+from pathlib import Path
 from typing import Any, Callable
 
 import toolz
@@ -40,6 +42,7 @@ from xorq.ibis_yaml.utils import (
 )
 from xorq.vendor.ibis.common.collections import FrozenOrderedDict
 from xorq.vendor.ibis.expr.operations.relations import Namespace
+from xorq.vendor.ibis.util import normalize_filenames
 
 
 def should_register_node(node_dict):
@@ -536,12 +539,27 @@ def _remotetable_from_yaml(yaml_dict: dict, context: any) -> ibis.Expr:
     return remote_table_expr
 
 
+def warn_on_local_path(items: dict) -> None:
+    items = dict(items)
+    if path := next((items[k] for k in ("path", "source") if k in items), None):
+        paths = normalize_filenames(path)
+        try:
+            if any(path.exists() for path in map(Path, paths)):
+                warnings.warn(
+                    "The Read op path is using a local filesystem path, running the build may not work in other environments."
+                )
+        except TypeError:
+            pass
+
+
 @translate_to_yaml.register(Read)
 def _read_to_yaml(op: Read, context: TranslationContext) -> dict:
     schema_id = context.schema_registry.register_schema(op.schema)
     profile_hash_name = (
         op.source._profile.hash_name if hasattr(op.source, "_profile") else None
     )
+
+    warn_on_local_path(op.read_kwargs)
 
     return freeze(
         {
