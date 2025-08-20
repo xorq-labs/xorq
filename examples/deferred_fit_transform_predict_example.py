d2480cdf8460b18df6534fd891c733470101b95a
Daniel Mesejo <mesejoleon@gmail.com>
2025-08-20 11:35:54 +0200

    chore: update hackernews_lib version

--- examples/deferred_fit_transform_predict_example.py
+++ examples/deferred_fit_transform_predict_example.py
@@ -18,7 +18,7 @@ from xorq.ml import (
 
 
 m = import_python(
-    xo.options.pins.get_path("hackernews_lib", version="20250604T223424Z-2e578")
+    xo.options.pins.get_path("hackernews_lib", version="20250820T111457Z-1d66a")
 )
 
 
