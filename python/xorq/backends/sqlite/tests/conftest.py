169687fbaf4475e8365565c17d6a84ec1632e19d
Daniel Mesejo <mesejoleon@gmail.com>
2025-08-28 12:01:07 +0200

    wip: fix error with test

--- python/xorq/backends/sqlite/tests/conftest.py
+++ python/xorq/backends/sqlite/tests/conftest.py
@@ -6,3 +6,8 @@ import xorq.api as xo
 @pytest.fixture(scope="session")
 def sqlite_con():
     return xo.sqlite.connect()
+
+
+@pytest.fixture(scope="session")
+def astronauts_parquet_path(parquet_dir):
+    return parquet_dir / "astronauts.parquet"
