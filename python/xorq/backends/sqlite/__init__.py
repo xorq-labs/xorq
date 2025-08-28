dbdd7e2951faebed633651fb6bcf851e30883190
Daniel Mesejo <mesejoleon@gmail.com>
2025-08-28 11:48:57 +0200

    wip: add caching, read_parquet, read_record_batches

--- python/xorq/backends/sqlite/__init__.py
+++ python/xorq/backends/sqlite/__init__.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
 import sqlglot as sg
 
+from xorq.expr.api import read_parquet
 from xorq.vendor.ibis import Schema
 from xorq.vendor.ibis.backends.sqlite import Backend as IbisSQLiteBackend
 from xorq.vendor.ibis.expr import types as ir
@@ -19,21 +21,41 @@ class Backend(IbisSQLiteBackend):
         self,
         record_batches: pa.RecordBatchReader,
         table_name: str | None = None,
+        mode: str = "create",
         **kwargs: Any,
     ) -> ir.Table:
+        from xorq.common.utils.sqlite_utils import SQLiteADBC
+
         table_name = table_name or gen_name("read_record_batches")
+
+        if ":memory:" in self.database:
+            self._into_memory_record_batches(record_batches, table_name)
+        else:
+            sqlite_adbc = SQLiteADBC(self)
+            sqlite_adbc.adbc_ingest(table_name, record_batches, mode=mode, **kwargs)
+
+        return self.table(table_name)
+
+    def _into_memory_record_batches(self, record_batches, table_name):
         schema = Schema.from_pyarrow(record_batches.schema)
         table = sg.table(table_name, quoted=self.compiler.quoted, catalog="temp")
         create_stmt = self._generate_create_table(table, schema).sql(self.name)
         df = record_batches.read_pandas()
-
         data = df.itertuples(index=False)
         insert_stmt = self._build_insert_template(
             table_name, schema=schema, catalog="temp", columns=True
         )
-
         with self.begin() as cur:
             cur.execute(create_stmt)
             cur.executemany(insert_stmt, data)
 
-        return self.table(table_name)
+    def read_parquet(
+        self,
+        path: str | Path,
+        table_name: str | None = None,
+        mode: str = "create",
+        **kwargs: Any,
+    ) -> ir.Table:
+        table_name = table_name or gen_name("xo_read_parquet")
+        record_batches = read_parquet(path).to_pyarrow_batches()
+        return self.read_record_batches(record_batches, table_name, mode, **kwargs)
--- python/xorq/common/utils/sqlite_utils.py
+++ python/xorq/common/utils/sqlite_utils.py
@@ -12,21 +12,23 @@ from xorq.backends.sqlite import (
 )
 
 
-@frozen
-class PgADBC:
-    con = field(validator=instance_of(SQLiteBackend))
+def get_sqlite_stats(dt):
+    (con, name) = (dt.source, dt.name)
+    sql = f"SELECT COUNT(*), MAX(id) FROM '{name}'"
+    (count, max_id) = con.sql(sql).execute()
+    return (count, max_id)
 
-    def get_uri(self, **kwargs):
-        params = {**self.params, **kwargs}
-        uri = f"postgresql://{params['user']}:{params['password']}@{params['host']}:{params['port']}/{params['database']}"
-        return uri
+
+@frozen
+class SQLiteADBC:
+    con = field(validator=instance_of(SQLiteBackend))
 
     @property
     def uri(self):
-        return self.get_uri()
+        return self.con.database
 
     def get_conn(self, **kwargs):
-        return adbc_driver_sqlite.dbapi.connect(self.get_uri(**kwargs))
+        return adbc_driver_sqlite.dbapi.connect(self.uri)
 
     @property
     def conn(self):
