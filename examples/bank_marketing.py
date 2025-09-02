ee437b5693b52a4b06187c45929a0b9dc96de807
Dan Lovell <dlovell@gmail.com>
2025-09-02 18:02:46 +0100

    fix(ml): add `storage` arg to `Pipeline.fit` (#1257)

--- examples/bank_marketing.py
+++ examples/bank_marketing.py
@@ -1,4 +1,5 @@
 import functools
+from pathlib import Path
 
 import pandas as pd
 import xgboost as xgb
@@ -17,6 +18,9 @@ from sklearn.preprocessing import (
 import xorq.api as xo
 import xorq.expr.selectors as s
 import xorq.vendor.ibis.expr.datatypes as dt
+from xorq.caching import (
+    ParquetStorage,
+)
 from xorq.common.utils.defer_utils import deferred_read_csv
 from xorq.expr.ml import (
     train_test_splits,
@@ -83,6 +87,7 @@ class XGBoostModelExplodeEncoded(BaseEstimator):
         self.model = xgb.train(
             self.params, dtrain, num_boost_round=self.num_boost_round
         )
+        print("fitting")
         return self
 
     def predict(self, X):
@@ -102,8 +107,8 @@ xgbee_step = Step(
 )
 
 
-def make_pipeline(dataset_name, target_column, predicted_col):
-    con = xo.connect()
+def make_pipeline(dataset_name, target_column, predicted_col, con=None, storage=None):
+    con = con or xo.connect()
     expr = deferred_read_csv(
         path=xo.options.pins.get_path(dataset_name),
         con=con,
@@ -128,12 +133,14 @@ def make_pipeline(dataset_name, target_column, predicted_col):
         train_table,
         features=train_table.select(s.of_type(str)).columns,
         dest_col=ENCODED,
+        storage=storage,
     )
     fitted_xgbee_step = xgbee_step.fit(
         expr=train_table.mutate(fitted_one_hot_step.mutate),
         features=numeric_features + (ENCODED,),
         target=target_column,
         dest_col=predicted_col,
+        storage=storage,
     )
     fitted_pipeline = FittedPipeline(
         (fitted_one_hot_step, fitted_xgbee_step),
@@ -142,13 +149,19 @@ def make_pipeline(dataset_name, target_column, predicted_col):
     return (train_table, test_table, fitted_pipeline)
 
 
+con = xo.connect()
+storage = ParquetStorage(
+    source=con,
+    relative_path="./tmp-cache",
+    base_path=Path(".").absolute(),
+)
 (dataset_name, target_column, predicted_col) = (
     "bank-marketing",
     "deposit",
     "predicted",
 )
 train_table, test_table, fitted_pipeline = make_pipeline(
-    dataset_name, target_column, predicted_col
+    dataset_name, target_column, predicted_col, con=con, storage=storage
 )
 encoded_test = fitted_pipeline.transform(test_table)
 predicted_test = fitted_pipeline.predict(test_table)
