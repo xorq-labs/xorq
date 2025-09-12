c69be48902d2359bee57ff65e7e34951e5b1c638
Daniel Mesejo <mesejoleon@gmail.com>
2025-09-12 06:00:02 +0200

    ref

--- python/xorq/tests/conftest.py
+++ python/xorq/tests/conftest.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 import pytest
 
@@ -10,6 +9,7 @@ from xorq.common.utils.aws_utils import (
     connection_is_set,
     make_s3_credentials_defaults,
 )
+from xorq.conftest import array_types_df
 from xorq.vendor import ibis
 
 
@@ -112,67 +112,6 @@ TEST_TABLES = {
         }
     ),
 }
-
-array_types_df = pd.DataFrame(
-    [
-        (
-            [np.int64(1), 2, 3],
-            ["a", "b", "c"],
-            [1.0, 2.0, 3.0],
-            "a",
-            1.0,
-            [[], [np.int64(1), 2, 3], None],
-        ),
-        (
-            [4, 5],
-            ["d", "e"],
-            [4.0, 5.0],
-            "a",
-            2.0,
-            [],
-        ),
-        (
-            [6, None],
-            ["f", None],
-            [6.0, np.nan],
-            "a",
-            3.0,
-            [None, [], None],
-        ),
-        (
-            [None, 1, None],
-            [None, "a", None],
-            [],
-            "b",
-            4.0,
-            [[1], [2], [], [3, 4, 5]],
-        ),
-        (
-            [2, None, 3],
-            ["b", None, "c"],
-            np.nan,
-            "b",
-            5.0,
-            None,
-        ),
-        (
-            [4, None, None, 5],
-            ["d", None, None, "e"],
-            [4.0, np.nan, np.nan, 5.0],
-            "c",
-            6.0,
-            [[1, 2, 3]],
-        ),
-    ],
-    columns=[
-        "x",
-        "y",
-        "z",
-        "grouper",
-        "scalar_column",
-        "multi_dim",
-    ],
-)
 
 have_s3_credentials = connection_is_set(make_s3_credentials_defaults())
 
