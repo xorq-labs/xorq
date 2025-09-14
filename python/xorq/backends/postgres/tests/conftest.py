dfabfda90a0718fdc2f81286d711a57b0f21bdb5
Daniel Mesejo <mesejoleon@gmail.com>
2025-09-14 08:08:31 +0200

    chore(postgres): organize testing harness

--- python/xorq/backends/postgres/tests/conftest.py
+++ python/xorq/backends/postgres/tests/conftest.py
@@ -1,1 +1,26 @@
-../../let/tests/conftest.py
+import pandas as pd
+import pytest
+
+import xorq.api as xo
+
+
+@pytest.fixture(scope="function")
+def con():
+    return xo.connect()
+
+
+@pytest.fixture(scope="session")
+def iris_path() -> str:
+    path = xo.options.pins.get_path("iris")
+    return path
+
+
+@pytest.fixture(scope="session")
+def astronauts_parquet_path(parquet_dir):
+    path = parquet_dir / "astronauts.parquet"
+    return path
+
+
+@pytest.fixture(scope="session")
+def astronauts_df(astronauts_parquet_path) -> pd.DataFrame:
+    return pd.read_parquet(astronauts_parquet_path)
