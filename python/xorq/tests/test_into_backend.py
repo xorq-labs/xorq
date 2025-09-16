07eef246a51fac87c4b243ad39c59e9a07b09d0d
Daniel Mesejo <mesejoleon@gmail.com>
2025-09-16 11:35:35 +0200

    chore: move caching and into_backend

--- python/xorq/tests/test_into_backend.py
+++ python/xorq/tests/test_into_backend.py
@@ -3,7 +3,6 @@ import random
 from operator import methodcaller
 from pathlib import PosixPath
 
-import dask
 import numpy as np
 import pandas as pd
 import pyarrow as pa
@@ -11,9 +10,8 @@ import pytest
 from pytest import param
 
 import xorq.api as xo
-from xorq.backends.conftest import get_storage_uncached
-from xorq.caching import ParquetStorage, SourceSnapshotStorage, SourceStorage
-from xorq.expr.relations import into_backend, register_and_transform_remote_tables
+from xorq.caching import ParquetStorage, SourceStorage
+from xorq.expr.relations import register_and_transform_remote_tables
 from xorq.loader import load_backend
 from xorq.tests.util import assert_frame_equal, check_eq
 from xorq.vendor import ibis
@@ -710,55 +708,3 @@ def test_multi_engine_cache(pg, ls_con, ls_batting, tmp_path, backend_name):
     )
 
     assert expr.execute() is not None
-
-
-def test_pandas_snapshot(ls_con, alltypes_df):
-    group_by = "year"
-    name = ibis.util.gen_name("tmp_table")
-
-    # create a temp table we can mutate
-    pd_con = xo.pandas.connect()
-    table = pd_con.create_table(name, alltypes_df)
-
-    cached_expr = (
-        table.group_by(group_by)
-        .agg({f"count_{col}": table[col].count() for col in table.columns})
-        .pipe(into_backend, ls_con)
-        .cache(storage=SourceSnapshotStorage(source=ls_con))
-    )
-    (storage, uncached) = get_storage_uncached(cached_expr)
-
-    # test preconditions
-    assert not storage.exists(uncached)
-
-    # test cache creation
-    executed0 = cached_expr.execute()
-
-    with storage.normalization_context(uncached):
-        normalized0 = dask.base.normalize_token(uncached)
-    assert storage.exists(uncached)
-
-    # test cache use
-    executed1 = cached_expr.execute()
-    assert executed0.equals(executed1)
-
-    # test NO cache invalidation
-    pd_con.reconnect()
-    table2 = pd_con.create_table(name, pd.concat((alltypes_df, alltypes_df)))
-
-    cached_expr = (
-        table2.group_by(group_by)
-        .agg({f"count_{col}": table2[col].count() for col in table2.columns})
-        .pipe(into_backend, ls_con)
-        .cache(storage)
-    )
-    (storage, uncached) = get_storage_uncached(cached_expr)
-    with storage.normalization_context(uncached):
-        normalized1 = dask.base.normalize_token(uncached)
-
-    # everything else is stable, despite the different data
-    assert normalized0[1][1] == normalized1[1][1]
-    assert storage.exists(uncached)
-    assert storage.get_key(uncached).count(KEY_PREFIX) == 1
-    executed2 = cached_expr.ls.uncached.execute()
-    assert not executed0.equals(executed2)
