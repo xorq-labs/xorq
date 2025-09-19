9c60c8844c4fccf522436da0b5ba765cf605fb82
Daniel Mesejo <mesejoleon@gmail.com>
2025-09-19 18:19:07 +0200

    ref: test pipeline_lib

--- python/xorq/expr/ml/tests/test_pipeline_lib.py
+++ python/xorq/expr/ml/tests/test_pipeline_lib.py
@@ -7,23 +7,31 @@ sklearn = pytest.importorskip("sklearn")
 
 
 TARGET = "target"
+features = (feature0, feature1) = ("feature_0", "feature_1")
 
 
-def test_infer_features():
-    features = (feature0, feature1) = ("feature_0", "feature_1")
-    t = xo.memtable(
+@pytest.fixture(scope="module")
+def t():
+    return xo.memtable(
         {
             feature0: [1, 2],
             feature1: [3, 4],
             TARGET: [0, 1],
         }
     )
+
+
+@pytest.fixture(scope="module")
+def fitted_xorq_pipeline(t):
     sklearn_pipeline = sklearn.pipeline.make_pipeline(
         sklearn.preprocessing.StandardScaler(),
         sklearn.linear_model.LinearRegression(),
     )
     xorq_pipeline = xo.Pipeline.from_instance(sklearn_pipeline)
-    fitted_xorq_pipeline = xorq_pipeline.fit(t, target=TARGET)
+    return xorq_pipeline.fit(t, target=TARGET)
+
+
+def test_infer_features(fitted_xorq_pipeline):
     assert all(
         features == step.features for step in fitted_xorq_pipeline.transform_steps
     )
@@ -60,22 +68,7 @@ def expected_tags(keys=None):
         ("predict",),
     ),
 )
-def test_tagging_pipeline(keys):
-    (feature0, feature1) = ("feature_0", "feature_1")
-    t = xo.memtable(
-        {
-            feature0: [1, 2],
-            feature1: [3, 4],
-            TARGET: [0, 1],
-        }
-    )
-    sklearn_pipeline = sklearn.pipeline.make_pipeline(
-        sklearn.preprocessing.StandardScaler(),
-        sklearn.linear_model.LinearRegression(),
-    )
-    xorq_pipeline = xo.Pipeline.from_instance(sklearn_pipeline)
-    fitted_xorq_pipeline = xorq_pipeline.fit(t, target=TARGET)
-
+def test_tagging_pipeline_with_metadata(keys, t, fitted_xorq_pipeline):
     def filter_by_keys(d):
         if not keys:
             return True
@@ -89,3 +82,7 @@ def test_tagging_pipeline(keys):
             with_metadata=True,
         )
     ) == expected_tags(keys)
+
+
+def test_tagging_pipeline(fitted_xorq_pipeline, t):
+    assert len(fitted_xorq_pipeline.predict(t).ls.tags) > 0
