67da95852fd494c208cd219c7c165cf0a9823097
Daniel Mesejo <mesejoleon@gmail.com>
2025-09-27 22:32:25 +0200

    feat: add tagging for ml ops (#1297)
    Co-authored-by: dlovell <dlovell@gmail.com>

--- python/xorq/expr/ml/tests/test_pipeline_lib.py
+++ python/xorq/expr/ml/tests/test_pipeline_lib.py
@@ -7,23 +7,83 @@ sklearn = pytest.importorskip("sklearn")
 
 
 TARGET = "target"
+features = (feature0, feature1) = ("feature_0", "feature_1")
 
 
-def test_infer_features():
-    features = (feature0, feature1) = ("feature_0", "feature_1")
-    t = xo.memtable(
+@pytest.fixture(scope="module")
+def t():
+    return xo.memtable(
         {
             feature0: [1, 2],
             feature1: [3, 4],
             TARGET: [0, 1],
         }
     )
+
+
+@pytest.fixture(scope="module")
+def fitted_xorq_pipeline(t):
     sklearn_pipeline = sklearn.pipeline.make_pipeline(
         sklearn.preprocessing.StandardScaler(),
         sklearn.linear_model.LinearRegression(),
     )
     xorq_pipeline = xo.Pipeline.from_instance(sklearn_pipeline)
-    fitted_xorq_pipeline = xorq_pipeline.fit(t, target=TARGET)
+    return xorq_pipeline.fit(t, target=TARGET)
+
+
+def test_infer_features(fitted_xorq_pipeline):
     assert all(
         features == step.features for step in fitted_xorq_pipeline.transform_steps
     )
+
+
+def expected_tags(keys=None):
+    ds = (
+        {
+            "tag": "FittedPipeline-predict",
+            "predict": (
+                ("copy_X", True),
+                ("fit_intercept", True),
+                ("n_jobs", None),
+                ("positive", False),
+                ("tol", 1e-06),
+            ),
+        },
+        {
+            "tag": "FittedPipeline-transform",
+            "transform": ("standardscaler",),
+        },
+    )
+    return tuple(d for d in ds if d.keys() & keys)
+
+
+@pytest.mark.parametrize(
+    "keys",
+    (
+        ("FittedPipeline-transform",),
+        (
+            "FittedPipeline.transform",
+            "FittedPipeline.predict",
+        ),
+        ("FittedPipeline-predict",),
+    ),
+)
+def test_tagging_pipeline_with_metadata(keys, t, fitted_xorq_pipeline):
+    def filter_by_keys(d, keys=keys):
+        if not keys:
+            return True
+        else:
+            return d.keys() & tuple(keys)
+
+    actual = tuple(
+        node.metadata
+        for node in fitted_xorq_pipeline.predict(t).ls.get_tags(
+            predicate=filter_by_keys,
+        )
+    )
+    expected = expected_tags(keys)
+    assert actual == expected
+
+
+def test_tagging_pipeline(fitted_xorq_pipeline, t):
+    assert len(fitted_xorq_pipeline.predict(t).ls.tags) > 0
