1b90597ebd2d02d6d5f62af33d09ce39abe3f105
Dan Lovell <dlovell@gmail.com>
2025-09-28 16:29:22 +0100

    fix(ml): fixup pipeline tagging tests (#1312)

--- python/xorq/expr/ml/tests/test_pipeline_lib.py
+++ python/xorq/expr/ml/tests/test_pipeline_lib.py
@@ -1,6 +1,10 @@
+import operator
+
 import pytest
 
 import xorq.api as xo
+from xorq.common.utils.graph_utils import walk_nodes
+from xorq.expr.relations import Tag
 
 
 sklearn = pytest.importorskip("sklearn")
@@ -8,6 +12,9 @@ sklearn = pytest.importorskip("sklearn")
 
 TARGET = "target"
 features = (feature0, feature1) = ("feature_0", "feature_1")
+
+
+get_metadata = operator.attrgetter("metadata")
 
 
 @pytest.fixture(scope="module")
@@ -21,12 +28,20 @@ def t():
     )
 
 
+step_typs = (
+    sklearn.preprocessing.StandardScaler,
+    sklearn.linear_model.LinearRegression,
+)
+
+
 @pytest.fixture(scope="module")
-def fitted_xorq_pipeline(t):
-    sklearn_pipeline = sklearn.pipeline.make_pipeline(
-        sklearn.preprocessing.StandardScaler(),
-        sklearn.linear_model.LinearRegression(),
-    )
+def sklearn_pipeline():
+    sklearn_pipeline = sklearn.pipeline.make_pipeline(*(typ() for typ in step_typs))
+    return sklearn_pipeline
+
+
+@pytest.fixture(scope="module")
+def fitted_xorq_pipeline(sklearn_pipeline, t):
     xorq_pipeline = xo.Pipeline.from_instance(sklearn_pipeline)
     return xorq_pipeline.fit(t, target=TARGET)
 
@@ -37,53 +52,51 @@ def test_infer_features(fitted_xorq_pipeline):
     )
 
 
-def expected_tags(keys=None):
-    ds = (
-        {
-            "tag": "FittedPipeline-predict",
-            "predict": (
-                ("copy_X", True),
-                ("fit_intercept", True),
-                ("n_jobs", None),
-                ("positive", False),
-                ("tol", 1e-06),
-            ),
-        },
-        {
-            "tag": "FittedPipeline-transform",
-            "transform": ("standardscaler",),
-        },
-    )
-    return tuple(d for d in ds if d.keys() & keys)
+@pytest.fixture(scope="module")
+def all_tags(t, fitted_xorq_pipeline):
+    expr = fitted_xorq_pipeline.predict(t)
+    all_tags = walk_nodes((Tag,), expr)
+    return all_tags
 
 
-@pytest.mark.parametrize(
-    "keys",
-    (
-        ("FittedPipeline-transform",),
-        (
-            "FittedPipeline.transform",
-            "FittedPipeline.predict",
-        ),
-        ("FittedPipeline-predict",),
-    ),
-)
-def test_tagging_pipeline_with_metadata(keys, t, fitted_xorq_pipeline):
-    def filter_by_keys(d, keys=keys):
-        if not keys:
-            return True
-        else:
-            return d.keys() & tuple(keys)
-
-    actual = tuple(
-        node.metadata
-        for node in fitted_xorq_pipeline.predict(t).ls.get_tags(
-            predicate=filter_by_keys,
-        )
-    )
-    expected = expected_tags(keys)
+def test_all_tags(t, fitted_xorq_pipeline, all_tags):
+    expr = fitted_xorq_pipeline.predict(t)
+    actual = tuple(map(get_metadata, expr.ls.get_tags()))
+    expected = tuple(map(get_metadata, all_tags))
     assert actual == expected
 
 
-def test_tagging_pipeline(fitted_xorq_pipeline, t):
-    assert len(fitted_xorq_pipeline.predict(t).ls.tags) > 0
+@pytest.mark.parametrize(
+    "pairs",
+    (
+        (("tag", "FittedStep-transform"),),
+        (("tag", "FittedStep-predict"),),
+        (
+            ("tag", "FittedStep-transform"),
+            ("tag", "FittedStep-predict"),
+        ),
+    ),
+)
+def test_tagging_pipeline(pairs, t, fitted_xorq_pipeline):
+    def contains_any_pairs(d, pairs=pairs):
+        return set(pairs).intersection(d.items())
+
+    def sort_and_tuplify(dcts):
+        return tuple(sorted(tuple(sorted(dct.items())) for dct in dcts))
+
+    actual = sort_and_tuplify(
+        map(
+            get_metadata,
+            fitted_xorq_pipeline.predict(t).ls.get_tags(
+                predicate=contains_any_pairs,
+            ),
+        )
+    )
+    expected = sort_and_tuplify(
+        dct
+        for dct in (
+            fitted_step.tag_kwargs for fitted_step in fitted_xorq_pipeline.fitted_steps
+        )
+        if contains_any_pairs(dct)
+    )
+    assert actual and actual == expected
