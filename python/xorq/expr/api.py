3427920a80cff219d7bb8f6bf9d522c06d533cba
Daniel Mesejo <mesejoleon@gmail.com>
2025-09-30 18:33:05 +0200

    chore: rewrite test for cached node

--- python/xorq/expr/api.py
+++ python/xorq/expr/api.py
@@ -31,7 +31,6 @@ from xorq.expr.relations import (
     Tag,
     register_and_transform_remote_tables,
 )
-from xorq.vendor.ibis.backends.sql.dialects import DataFusion
 from xorq.vendor.ibis.expr import api
 from xorq.vendor.ibis.expr.api import *  # noqa: F403
 from xorq.vendor.ibis.expr.sql import SQLString
@@ -209,7 +208,7 @@ def read_postgres(
 def _cached_with_op(op, pretty, compiler):
     expr = op.to_expr()
     sg_expr = compiler.to_sqlglot(expr)
-    sql = sg_expr.sql(dialect=DataFusion, pretty=pretty)
+    sql = sg_expr.sql(dialect=compiler.dialect, pretty=pretty)
     return sql
 
 
@@ -238,20 +237,11 @@ def to_sql(expr: ir.Expr, compiler=None, pretty: bool = True) -> SQLString:
         Formatted SQL string
 
     """
-    from xorq.expr.relations import replace_cache_table
 
     if compiler is None:
         compiler = get_compiler(expr)
 
-    # FIXME verify why we would need to replace the CachedNode and update replace_cache_table with new method
-    unbound = (
-        _remove_tag_nodes(expr)
-        .op()
-        .replace(replace_cache_table)
-        .to_expr()
-        .unbind()
-        .op()
-    )
+    unbound = _remove_tag_nodes(expr).unbind().op()
     return SQLString(_cached_with_op(unbound, pretty, compiler))
 
 
