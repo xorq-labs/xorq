3739586fc3caa0c3142c74e37c64c38c288f3f24
dlovell <dlovell@gmail.com>
2025-10-04 17:41:25 -0400

    test(env_utils): add failing test for multiline env vars

--- python/xorq/common/utils/tests/test_env_utils.py
+++ python/xorq/common/utils/tests/test_env_utils.py
@@ -3,7 +3,10 @@ from pathlib import Path
 
 import pytest
 
-from xorq.common.utils.env_utils import EnvConfigable
+from xorq.common.utils.env_utils import (
+    EnvConfigable,
+    parse_env_file,
+)
 
 
 def test_subclass_from_kwargs(monkeypatch):
@@ -33,17 +36,24 @@ def test_subclass_from_kwargs(monkeypatch):
     assert env_config.SHOULDNT_EXIST == ""
 
 
-def test_subclass_from_env_file(monkeypatch, tmp_path):
-    content = "\n".join(
-        (
-            "export X=2",
-            "Y=3",
-            "a line that does not match, perhaps we should raise?",
-        )
-    )
+def make_EnvConfig_from_content(tmp_path, content):
     env_file = Path(tmp_path).joinpath(".env")
     env_file.write_text(content)
     EnvConfig = EnvConfigable.subclass_from_env_file(env_file)
+    return EnvConfig
+
+
+def test_subclass_from_env_file(monkeypatch, tmp_path):
+    EnvConfig = make_EnvConfig_from_content(
+        tmp_path,
+        "\n".join(
+            (
+                "export X=2",
+                "Y=3",
+                "a line that does not match, perhaps we should raise?",
+            )
+        ),
+    )
 
     env_config = EnvConfig()
     assert env_config.X == "2"
@@ -57,3 +67,23 @@ def test_subclass_from_env_file(monkeypatch, tmp_path):
     env_config = EnvConfig.from_env()
     assert env_config.X == "1"
     assert env_config.Y == "3"
+
+
+def test_single_line_env(tmp_path):
+    EnvConfig = make_EnvConfig_from_content(
+        tmp_path,
+        "x=1",
+    )
+    env_config = EnvConfig()
+    assert env_config.x == "1"
+
+
+def test_parse_multiline_env_vars(fixture_dir):
+    multiline_env_vars_path = fixture_dir.joinpath("multiline_env_vars.env")
+    dct = parse_env_file(multiline_env_vars_path)
+    actual = {k: len(v.split("\n")) for k, v in dct.items()}
+    expected = {
+        k: int(v)
+        for k, v in (el.split("=") for el in dct["names_to_numlines"].split(","))
+    }
+    assert actual == expected
