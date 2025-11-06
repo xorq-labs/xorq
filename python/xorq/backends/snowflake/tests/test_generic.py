e5376b948a30aedb80b9d3eee33131fc66a896c0
Daniel Mesejo <mesejoleon@gmail.com>
2025-11-06 22:53:16 +0100

    feat(snowflake): add hexdigest function

---
+++ python/xorq/backends/snowflake/tests/test_generic.py
@@ -0,0 +1,30 @@
+import pytest
+
+import xorq.api as xo
+from xorq.backends.snowflake.tests.conftest import inside_temp_schema
+from xorq.tests.util import assert_series_equal
+
+
+@pytest.mark.snowflake
+def test_create_table_from_expr_success(sf_con, temp_catalog, temp_db, parquet_dir):
+    with inside_temp_schema(sf_con, temp_catalog, temp_db):
+        name = "functional_alltypes"
+        alltypes = (
+            xo.deferred_read_parquet(
+                parquet_dir.joinpath(f"{name}.parquet"), table_name=name
+            )
+            .limit(1000)
+            .into_backend(sf_con)
+        )
+
+        h1 = alltypes.order_by("id").string_col.hexdigest().execute(limit=10)
+        df = alltypes.order_by("id").execute(limit=10)
+
+        import hashlib
+
+        def hash_256(col):
+            return hashlib.sha256(col.encode()).hexdigest()
+
+        h2 = df["string_col"].apply(hash_256).rename("HexDigest(string_col)")
+
+        assert_series_equal(h1, h2)
--- python/xorq/vendor/ibis/backends/sql/compilers/snowflake.py
+++ python/xorq/vendor/ibis/backends/sql/compilers/snowflake.py
@@ -893,5 +893,17 @@ $$""",
     def visit_ArrayMean(self, op, *, arg):
         return self.cast(self.f.udf.array_avg(arg), op.dtype)
 
+    def visit_HexDigest(self, op, *, arg, how):
+        if how == "md5":
+            return self.f.md5_hex(arg)
+        elif how == "sha1":
+            return self.f.sha1_hex(arg)
+        elif how == "sha256":
+            return self.f.sha2_hex(arg, 256)
+        elif how == "sha512":
+            return self.f.sha2_hex(arg, 512)
+        else:
+            raise com.UnsupportedOperationError(f"Unrecognized how argument: {how}. ")
+
 
 compiler = SnowflakeCompiler()
