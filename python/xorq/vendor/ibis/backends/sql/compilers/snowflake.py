ad728d0b8fb873d4bb60657566e25b05459ea204
Daniel Mesejo <mesejoleon@gmail.com>
2025-11-10 14:48:59 +0100

    feat(snowflake): add hexdigest function (#1374)
    Co-authored-by: dlovell <dlovell@gmail.com>

--- python/xorq/vendor/ibis/backends/sql/compilers/snowflake.py
+++ python/xorq/vendor/ibis/backends/sql/compilers/snowflake.py
@@ -893,5 +893,20 @@ $$""",
     def visit_ArrayMean(self, op, *, arg):
         return self.cast(self.f.udf.array_avg(arg), op.dtype)
 
+    def visit_HexDigest(self, op, *, arg, how):
+        match how:
+            case "md5":
+                return self.f.md5_hex(arg)
+            case "sha1":
+                return self.f.sha1_hex(arg)
+            case "sha256":
+                return self.f.sha2_hex(arg, 256)
+            case "sha512":
+                return self.f.sha2_hex(arg, 512)
+            case _:
+                raise com.UnsupportedOperationError(
+                    f"Unrecognized how argument: {how}. "
+                )
+
 
 compiler = SnowflakeCompiler()
