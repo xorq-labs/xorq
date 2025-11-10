fdd83e74a327a0c48c6e59ae8ed101dfb642ff07
dlovell <dlovell@gmail.com>
2025-11-10 07:58:39 -0500

    ref

--- python/xorq/backends/snowflake/tests/test_generic.py
+++ python/xorq/backends/snowflake/tests/test_generic.py
@@ -1,3 +1,5 @@
+import hashlib
+
 import pytest
 
 import xorq.api as xo
@@ -5,10 +7,15 @@ from xorq.backends.snowflake.tests.conftest import inside_temp_schema
 from xorq.tests.util import assert_series_equal
 
 
+def calc_hexdigest(string, how):
+    return hashlib.new(how, string.encode()).hexdigest()
+
+
 @pytest.mark.snowflake
 def test_create_table_from_expr_success(sf_con, temp_catalog, temp_db, parquet_dir):
     with inside_temp_schema(sf_con, temp_catalog, temp_db):
         name = "functional_alltypes"
+        col_name, by = "string_col", "id"
         alltypes = (
             xo.deferred_read_parquet(
                 parquet_dir.joinpath(f"{name}.parquet"), table_name=name
@@ -17,15 +24,8 @@ def test_create_table_from_expr_success(sf_con, temp_catalog, temp_db, parquet_d
             .into_backend(sf_con)
         )
 
-        import hashlib
-
+        expr = alltypes.order_by(by).limit(10)[col_name]
         for how in ("sha256", "sha512", "md5"):
-
-            def hashing(col):
-                return getattr(hashlib, how)(col.encode()).hexdigest()
-
-            h1 = alltypes.order_by("id").string_col.hexdigest(how=how).execute(limit=10)
-            df = alltypes.order_by("id").execute(limit=10)
-            h2 = df["string_col"].apply(hashing).rename("HexDigest(string_col)")
-
+            h1 = expr.hexdigest(how=how).execute()
+            h2 = expr.execute().apply(calc_hexdigest, how=how).rename(h1.name)
             assert_series_equal(h1, h2)
