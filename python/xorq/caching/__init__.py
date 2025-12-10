c51b8dd0706af5aaada398360f577978e859553d
dlovell <dlovell@gmail.com>
2025-12-10 06:37:42 -0500

    ref: split out caching.storage, caching.strategy

--- python/xorq/caching/__init__.py
+++ python/xorq/caching/__init__.py
@@ -1,80 +1,31 @@
 from __future__ import annotations
 
-import contextlib
-import datetime
 import functools
-from abc import (
-    abstractmethod,
-)
-from pathlib import (
-    Path,
-)
 
-import dask
 from attr import (
     field,
     frozen,
 )
 from attr.validators import (
     instance_of,
-    optional,
 )
 from opentelemetry import trace
 from public import public
 
-import xorq.common.utils.dask_normalize  # noqa: F401
-import xorq.vendor.ibis.expr.operations as ops
-from xorq.common.utils.caching_utils import (
-    get_xorq_cache_dir,
+from xorq.caching.storage import (
+    CacheStorage,
+    ParquetStorage,
+    ParquetTTLStorage,
+    SourceStorage,
 )
-from xorq.common.utils.dask_normalize.dask_normalize_expr import (
-    normalize_backend,
-    normalize_read,
-    normalize_remote_table,
-)
-from xorq.common.utils.dask_normalize.dask_normalize_utils import (
-    patch_normalize_token,
-)
-from xorq.common.utils.defer_utils import (
-    deferred_read_parquet,
-)
-from xorq.common.utils.func_utils import (
-    if_not_none,
+from xorq.caching.strategy import (
+    CacheStrategy,
+    ModificationTimeStrategy,
+    SnapshotStrategy,
 )
 from xorq.common.utils.otel_utils import tracer
-from xorq.config import _backend_init, options
-from xorq.expr.relations import (
-    Read,
-    RemoteTable,
-)
-from xorq.vendor import ibis
+from xorq.config import options
 from xorq.vendor.ibis.expr import types as ir
-
-
-@frozen
-class CacheStrategy:
-    @abstractmethod
-    def calc_key(self, expr):
-        pass
-
-
-@frozen
-class CacheStorage:
-    @abstractmethod
-    def exists(self, key):
-        pass
-
-    @abstractmethod
-    def get(self, key):
-        pass
-
-    @abstractmethod
-    def put(self, key, value):
-        pass
-
-    @abstractmethod
-    def drop(self, key):
-        pass
 
 
 @frozen
@@ -85,6 +36,13 @@ class Cache:
         validator=instance_of(str),
         factory=functools.partial(options.get, "cache.key_prefix"),
     )
+
+    strategy_typ = None
+    storage_typ = None
+
+    def __attrs_post_init__(self):
+        assert isinstance(self.strategy, self.strategy_typ)
+        assert isinstance(self.storage, self.storage_typ)
 
     def calc_key(self, expr):
         # the order here matters: must check is_cached before calling maybe_prevent_cross_source_caching
@@ -135,208 +93,34 @@ class Cache:
         else:
             self.storage.drop(key)
 
+    @classmethod
+    def from_kwargs(cls, **kwargs):
+        strategy = cls.strategy_typ()
+        storage = cls.storage_typ(**kwargs)
+        return cls(strategy=strategy, storage=storage)
 
+
+@public
 @frozen
-class ModificationTimeStrategy(CacheStrategy):
-    key_prefix = field(
-        validator=instance_of(str),
-        factory=functools.partial(options.get, "cache.key_prefix"),
-    )
+class ParquetCache(Cache):
+    """Cache expressions as Parquet files using a snapshot invalidation strategy.
 
-    def calc_key(self, expr: ir.Expr):
-        return expr.ls.tokenized
+    This storage class saves intermediate results as Parquet files in a specified
+    directory and uses a snapshot-based approach for cache invalidation.
+    The snapshot strategy ensures cached data is only invalidated when the
+    expression's definition changes, making it suitable for stable datasets.
 
+    Parameters
+    ----------
+    source : ibis.backends.BaseBackend
+        The backend to use for execution. Defaults to xorq's default backend.
+    path : Path
+        The directory where Parquet files will be stored. Defaults to
+        xorq.config.options.cache.default_path.
+    """
 
-@frozen
-class SnapshotStrategy(CacheStrategy):
-    def calc_key(self, expr: ir.Expr):
-        # can we cache this?
-        with self.normalization_context(expr):
-            replaced = self.replace_remote_table(expr)
-            tokenized = replaced.ls.tokenized
-            return "-".join(("snapshot", tokenized))
-
-    @contextlib.contextmanager
-    def normalization_context(self, expr):
-        typs = map(type, expr.ls.backends)
-        with patch_normalize_token(*typs, f=self.normalize_backend):
-            with patch_normalize_token(
-                ops.DatabaseTable,
-                f=self.normalize_databasetable,
-            ):
-                with patch_normalize_token(
-                    Read,
-                    f=self.cached_normalize_read,
-                ):
-                    yield
-
-    @staticmethod
-    @functools.cache
-    def cached_normalize_read(op):
-        return normalize_read(op)
-
-    @staticmethod
-    @functools.cache
-    def cached_replace_remote_table(op):
-        def rename_remote_table(node, _, **kwargs):
-            if isinstance(node, RemoteTable):
-                # FIXME: how to verify that we're always within a self.normalization_context?
-                name = dask.base.tokenize(node)
-                rt = RemoteTable(
-                    name=name,
-                    schema=node.schema,
-                    source=node.source,
-                    remote_expr=node.remote_expr,
-                    namespace=node.namespace,
-                )
-                return rt
-            else:
-                return node.__recreate__(kwargs)
-
-        return op.replace(rename_remote_table)
-
-    def replace_remote_table(self, expr):
-        """replace remote table with deterministic name ***strictly for key calculation***"""
-        if expr.op().find(RemoteTable):
-            expr = self.cached_replace_remote_table(expr.op()).to_expr()
-        return expr
-
-    @staticmethod
-    def normalize_backend(con):
-        name = con.name
-        if name in ("pandas", "duckdb", "datafusion", "let"):
-            return (name, None)
-        else:
-            return normalize_backend(con)
-
-    @staticmethod
-    def normalize_databasetable(dt):
-        if isinstance(dt, RemoteTable):
-            # one alternative is to explicitly iterate over the fields name, schema, source, namespace
-            # but explicit is better than implicit, additionally the name is not a safe bet for caching
-            # RemoteTable
-            return normalize_remote_table(dt)
-        else:
-            keys = ["name", "schema", "source", "namespace"]
-            return dask.tokenize._normalize_seq_func(
-                (key, getattr(dt, key)) for key in keys
-            )
-
-
-@frozen
-class ParquetStorage(CacheStorage):
-    source = field(
-        validator=instance_of(ibis.backends.BaseBackend),
-        factory=_backend_init,
-    )
-    relative_path = field(
-        validator=instance_of(Path),
-        factory=functools.partial(options.get, "cache.default_relative_path"),
-        converter=Path,
-    )
-    base_path = field(
-        validator=optional(instance_of(Path)),
-        default=None,
-        converter=if_not_none(Path),
-    )
-
-    def __attrs_post_init__(self):
-        self.path.mkdir(exist_ok=True, parents=True)
-
-    @property
-    def path(self):
-        return (self.base_path or get_xorq_cache_dir()).joinpath(self.relative_path)
-
-    def get_path(self, key):
-        return self.path.joinpath(key + ".parquet")
-
-    def exists(self, key):
-        return self.get_path(key).exists()
-
-    def get(self, key):
-        op = deferred_read_parquet(
-            path=self.get_path(key),
-            con=self.source,
-            table_name=key,
-        ).op()
-        return op
-
-    def put(self, key, value):
-        path = self.get_path(key)
-        # move from temp location upon success to prevent empty files on failure
-        tmp_path = path.with_name(path.name + ".tmp")
-        value.to_expr().to_parquet(tmp_path)
-        tmp_path.rename(path)
-        return self.get(key)
-
-    def drop(self, key):
-        path = self.get_path(key)
-        path.unlink()
-
-
-@frozen
-class ParquetTTLStorage(ParquetStorage):
-    ttl = field(
-        validator=instance_of(datetime.timedelta), default=datetime.timedelta(days=1)
-    )
-
-    def exists(self, key):
-        path = self.get_path(key)
-        return path.exists() and self.satisfies_ttl(path)
-
-    def satisfies_ttl(self, path):
-        delta = datetime.datetime.now() - datetime.datetime.fromtimestamp(
-            path.stat().st_mtime
-        )
-        return delta < self.ttl
-
-
-# named with underscore prefix until we swap out SourceStorage
-@frozen
-class SourceStorage(CacheStorage):
-    source = field(
-        validator=instance_of(ibis.backends.BaseBackend),
-        factory=_backend_init,
-    )
-
-    def exists(self, key):
-        return key in self.source.tables
-
-    def get(self, key):
-        return self.source.table(key).op()
-
-    def put(self, key, value):
-        def is_remote(value):
-            name = value.to_expr()._find_backend().name
-            # FIXME: add pyiceberg, trino
-            return name in ("postgres", "snowflake")
-
-        def is_single_backend(storage, value):
-            from xorq.common.utils.graph_utils import find_all_sources
-
-            return (storage.source,) == find_all_sources(value.to_expr())
-
-        if is_remote(value):
-            if is_single_backend(self, value):
-                from xorq.expr.api import _transform_expr
-
-                # must transform for Read ops: create_table expects a vanilla ibis expr
-                (transformed, _) = _transform_expr(value.to_expr())
-                self.source.create_table(key, transformed)
-            else:
-                assert hasattr(self.source, "read_record_batches")
-                # read_record_batches will create durable table in out-of-core fashion
-                # works for snowflake and postgres
-                self.source.read_record_batches(
-                    value.to_expr().to_pyarrow_batches(),
-                    key,
-                )
-        else:
-            self.source.create_table(key, value.to_expr().to_pyarrow())
-        return self.get(key)
-
-    def drop(self, key):
-        self.source.drop_table(key)
+    strategy_typ = ModificationTimeStrategy
+    storage_typ = ParquetStorage
 
 
 @public
@@ -358,62 +142,37 @@ class ParquetSnapshotCache(Cache):
         xorq.config.options.cache.default_path.
     """
 
-    def __attrs_post_init__(self):
-        assert isinstance(self.strategy, SnapshotStrategy)
-        assert isinstance(self.storage, ParquetStorage)
-
-    @classmethod
-    def from_kwargs(cls, **kwargs):
-        strategy = SnapshotStrategy()
-        storage = ParquetStorage(**kwargs)
-        return cls(strategy=strategy, storage=storage)
+    strategy_typ = SnapshotStrategy
+    storage_typ = ParquetStorage
 
 
 @public
 @frozen
 class ParquetTTLSnapshotCache(Cache):
-    def __attrs_post_init__(self):
-        assert isinstance(self.strategy, SnapshotStrategy)
-        assert isinstance(self.storage, ParquetTTLStorage)
-
-    @classmethod
-    def from_kwargs(cls, **kwargs):
-        strategy = SnapshotStrategy()
-        storage = ParquetTTLStorage(**kwargs)
-        return cls(strategy=strategy, storage=storage)
+    strategy_typ = SnapshotStrategy
+    storage_typ = ParquetTTLStorage
 
 
 @public
 @frozen
-class SourceCache:
-    def __attrs_post_init__(self):
-        assert isinstance(self.strategy, ModificationTimeStrategy)
-        assert isinstance(self.storage, SourceStorage)
-
-    @classmethod
-    def from_kwargs(cls, key_prefix, source):
-        strategy = ModificationTimeStrategy(key_prefix=key_prefix)
-        storage = SourceStorage(source=source)
-        return cls(strategy=strategy, storage=storage)
+class SourceCache(Cache):
+    strategy_typ = ModificationTimeStrategy
+    storage_typ = SourceStorage
 
 
 @public
 @frozen
-class SourceSnapshotCache:
-    def __attrs_post_init__(self):
-        assert isinstance(self.strategy, SnapshotStrategy)
-        assert isinstance(self.storage, SourceStorage)
-
-    @classmethod
-    def from_kwargs(cls, source):
-        strategy = SnapshotStrategy()
-        storage = SourceStorage(source=source)
-        return cls(strategy=strategy, storage=storage)
+class SourceSnapshotCache(Cache):
+    strategy_typ = SnapshotStrategy
+    storage_typ = SourceStorage
 
 
 @public
 @frozen
 class GCSCache(Cache):
+    strategy_typ = ModificationTimeStrategy
+    storage_typ = None
+
     def __attrs_post_init__(self):
         from xorq.common.utils.gcloud_utils import _GCStorage
 
@@ -424,7 +183,7 @@ class GCSCache(Cache):
     def from_kwargs(cls, bucket_name, source):
         from xorq.common.utils.gcloud_utils import _GCStorage
 
-        strategy = ModificationTimeStrategy()
+        strategy = cls.strategy_typ()
         storage = _GCStorage(bucket_name=bucket_name, source=source)
         return cls(strategy=strategy, storage=storage)
 
---
+++ python/xorq/caching/storage.py
@@ -0,0 +1,166 @@
+from __future__ import annotations
+
+import datetime
+import functools
+from abc import (
+    abstractmethod,
+)
+from pathlib import (
+    Path,
+)
+
+from attr import (
+    field,
+    frozen,
+)
+from attr.validators import (
+    instance_of,
+    optional,
+)
+
+from xorq.common.utils.caching_utils import (
+    get_xorq_cache_dir,
+)
+from xorq.common.utils.defer_utils import (
+    deferred_read_parquet,
+)
+from xorq.common.utils.func_utils import (
+    if_not_none,
+)
+from xorq.config import _backend_init, options
+from xorq.vendor import ibis
+
+
+@frozen
+class CacheStorage:
+    @abstractmethod
+    def exists(self, key):
+        pass
+
+    @abstractmethod
+    def get(self, key):
+        pass
+
+    @abstractmethod
+    def put(self, key, value):
+        pass
+
+    @abstractmethod
+    def drop(self, key):
+        pass
+
+
+@frozen
+class ParquetStorage(CacheStorage):
+    source = field(
+        validator=instance_of(ibis.backends.BaseBackend),
+        factory=_backend_init,
+    )
+    relative_path = field(
+        validator=instance_of(Path),
+        factory=functools.partial(options.get, "cache.default_relative_path"),
+        converter=Path,
+    )
+    base_path = field(
+        validator=optional(instance_of(Path)),
+        default=None,
+        converter=if_not_none(Path),
+    )
+
+    def __attrs_post_init__(self):
+        self.path.mkdir(exist_ok=True, parents=True)
+
+    @property
+    def path(self):
+        return (self.base_path or get_xorq_cache_dir()).joinpath(self.relative_path)
+
+    def get_path(self, key):
+        return self.path.joinpath(key + ".parquet")
+
+    def exists(self, key):
+        return self.get_path(key).exists()
+
+    def get(self, key):
+        op = deferred_read_parquet(
+            path=self.get_path(key),
+            con=self.source,
+            table_name=key,
+        ).op()
+        return op
+
+    def put(self, key, value):
+        path = self.get_path(key)
+        # move from temp location upon success to prevent empty files on failure
+        tmp_path = path.with_name(path.name + ".tmp")
+        value.to_expr().to_parquet(tmp_path)
+        tmp_path.rename(path)
+        return self.get(key)
+
+    def drop(self, key):
+        path = self.get_path(key)
+        path.unlink()
+
+
+@frozen
+class ParquetTTLStorage(ParquetStorage):
+    ttl = field(
+        validator=instance_of(datetime.timedelta), default=datetime.timedelta(days=1)
+    )
+
+    def exists(self, key):
+        path = self.get_path(key)
+        return path.exists() and self.satisfies_ttl(path)
+
+    def satisfies_ttl(self, path):
+        delta = datetime.datetime.now() - datetime.datetime.fromtimestamp(
+            path.stat().st_mtime
+        )
+        return delta < self.ttl
+
+
+# named with underscore prefix until we swap out SourceStorage
+@frozen
+class SourceStorage(CacheStorage):
+    source = field(
+        validator=instance_of(ibis.backends.BaseBackend),
+        factory=_backend_init,
+    )
+
+    def exists(self, key):
+        return key in self.source.tables
+
+    def get(self, key):
+        return self.source.table(key).op()
+
+    def put(self, key, value):
+        def is_remote(value):
+            name = value.to_expr()._find_backend().name
+            # FIXME: add pyiceberg, trino
+            return name in ("postgres", "snowflake")
+
+        def is_single_backend(storage, value):
+            from xorq.common.utils.graph_utils import find_all_sources
+
+            return (storage.source,) == find_all_sources(value.to_expr())
+
+        if is_remote(value):
+            if is_single_backend(self, value):
+                from xorq.expr.api import _transform_expr
+
+                # must transform for Read ops: create_table expects a vanilla ibis expr
+                (transformed, _) = _transform_expr(value.to_expr())
+                self.source.create_table(key, transformed)
+            else:
+                assert hasattr(self.source, "read_record_batches")
+                # read_record_batches will create durable table in out-of-core fashion
+                # works for snowflake and postgres
+                self.source.read_record_batches(
+                    value.to_expr().to_pyarrow_batches(),
+                    key,
+                )
+        else:
+            self.source.create_table(key, value.to_expr().to_pyarrow())
+        return self.get(key)
+
+    def drop(self, key):
+        self.source.drop_table(key)
