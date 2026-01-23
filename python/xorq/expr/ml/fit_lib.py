c9bc19c71cc92aca4f2a094cd4bd39cdebdf3d21
George Hoersting <ghoersti@Georges-MacBook-Pro.local>
2026-01-23 12:45:06 -0500

    chore: remove cloud pickle from params replace , remove _make_name, use make_name

--- python/xorq/expr/ml/fit_lib.py
+++ python/xorq/expr/ml/fit_lib.py
@@ -2,7 +2,6 @@ import functools
 from typing import Callable
 
 import cloudpickle
-import dask.base
 import numpy as np
 import pyarrow as pa
 import toolz
@@ -26,16 +25,14 @@ from xorq.vendor.ibis.expr.types.core import Expr
 
 
 @toolz.curry
-def fit_sklearn(df, target=None, *, cls, params_pickled):
-    params = cloudpickle.loads(params_pickled)
+def fit_sklearn(df, target=None, *, cls, params):
     obj = cls(**dict(params))
     obj.fit(df, target)
     return obj
 
 
 @toolz.curry
-def fit_sklearn_series(df, col, cls, params_pickled):
-    params = cloudpickle.loads(params_pickled)
+def fit_sklearn_series(df, col, cls, params):
     model = cls(**dict(params))
     model.fit(df[col])
     return model
@@ -64,8 +61,7 @@ def transform_sklearn_feature_names_out(model, df):
 
 
 @toolz.curry
-def fit_sklearn_struct(df, *args, cls, params_pickled):
-    params = cloudpickle.loads(params_pickled)
+def fit_sklearn_struct(df, *args, cls, params):
     instance = cls(**dict(params))
     instance.fit(df, *args)
     return instance
@@ -143,11 +139,6 @@ def _maybe_decode_encoded_columns(df, features, encoded_cols):
     return result_df, tuple(new_features)
 
 
-def _make_name(prefix, to_tokenize, n=32):
-    tokenized = dask.base.tokenize(to_tokenize)
-    return ("_" + prefix + "_" + tokenized)[:n].lower()
-
-
 @frozen
 class DeferredFitOther:
     expr = field(validator=instance_of(Expr))
@@ -206,7 +197,7 @@ class DeferredFitOther:
             ),
             schema=self.fit_schema,
             return_type=dt.binary,
-            name=_make_name(f"fit_{self.name_infix}", (self.fit, self.other)),
+            name=make_name(f"fit_{self.name_infix}", (self.fit, self.other)),
         )
 
     @property
@@ -224,7 +215,7 @@ class DeferredFitOther:
             fn=fn,
             schema=self.schema,
             return_type=return_type,
-            name=_make_name(self.name_infix, (self.fit, self.other)),
+            name=make_name(self.name_infix, (self.fit, self.other)),
         )
         return deferred_other
 
@@ -280,12 +271,11 @@ def deferred_fit_other_sklearn(
     params=(),
     cache=None,
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=target,
         features=features,
-        fit=fit_sklearn(cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn(cls=cls, params=params),
         other=other,
         return_type=return_type,
         name_infix=name_infix,
@@ -328,12 +318,11 @@ def deferred_fit_predict(
     name_infix="predict",
     cache=None,
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=target,
         features=features,
-        fit=fit_sklearn(cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn(cls=cls, params=params),
         other=predict_sklearn,
         return_type=return_type,
         name_infix=name_infix,
@@ -352,12 +341,11 @@ def deferred_fit_transform_sklearn(
     name_infix="transformed",
     cache=None,
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=target,
         features=features,
-        fit=fit_sklearn(cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn(cls=cls, params=params),
         other=transform_sklearn,
         return_type=return_type,
         name_infix=name_infix,
@@ -376,12 +364,11 @@ def deferred_fit_predict_sklearn(
     name_infix="predicted",
     cache=None,
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=target,
         features=features,
-        fit=fit_sklearn(cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn(cls=cls, params=params),
         other=predict_sklearn,
         return_type=return_type,
         name_infix=name_infix,
@@ -400,12 +387,11 @@ def deferred_fit_predict_proba_sklearn(
     name_infix="predicted_proba",
     cache=None,
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=target,
         features=features,
-        fit=fit_sklearn(cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn(cls=cls, params=params),
         other=predict_proba_sklearn,
         return_type=return_type,
         name_infix=name_infix,
@@ -424,12 +410,11 @@ def deferred_fit_decision_function_sklearn(
     name_infix="decision_function",
     cache=None,
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=target,
         features=features,
-        fit=fit_sklearn(cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn(cls=cls, params=params),
         other=decision_function_sklearn,
         return_type=return_type,
         name_infix=name_infix,
@@ -448,12 +433,11 @@ def deferred_fit_feature_importances_sklearn(
     name_infix="feature_importances_",
     cache=None,
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=target,
         features=features,
-        fit=fit_sklearn(cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn(cls=cls, params=params),
         other=feature_importances_sklearn,
         return_type=return_type,
         name_infix=name_infix,
@@ -465,12 +449,11 @@ def deferred_fit_feature_importances_sklearn(
 def deferred_fit_transform_series_sklearn(
     expr, col, cls, return_type, params=(), name="predicted", cache=None
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=None,
         features=(col,),
-        fit=fit_sklearn_series(col=col, cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn_series(col=col, cls=cls, params=params),
         other=transform_sklearn_series(col=col),
         return_type=return_type,
         name_infix=name,
@@ -482,12 +465,11 @@ def deferred_fit_transform_series_sklearn(
 def deferred_fit_transform_series_sklearn_packed(
     expr, col, cls, params=(), name="transformed_packed", cache=None
 ):
-    params_pickled = cloudpickle.dumps(params)
     return DeferredFitOther(
         expr=expr,
         target=None,
         features=(col,),
-        fit=fit_sklearn_series(col=col, cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn_series(col=col, cls=cls, params=params),
         other=transform_sklearn_series_kv(col=col),
         return_type=KV_ENCODED_TYPE,
         name_infix=name,
@@ -499,12 +481,11 @@ def deferred_fit_transform_series_sklearn_packed(
 def deferred_fit_transform_sklearn_struct(
     expr, features, cls, params=(), target=None, name_infix="transformed", cache=None
 ):
-    params_pickled = cloudpickle.dumps(params)
     structer = Structer.from_instance_expr(cls(**dict(params)), expr, features=features)
     return DeferredFitOther(
         expr=expr,
         features=list(features),
-        fit=fit_sklearn_struct(cls=cls, params_pickled=params_pickled),
+        fit=fit_sklearn_struct(cls=cls, params=params),
         other=transform_sklearn_struct(structer.get_convert_array()),
         return_type=structer.return_type,
         target=target,
@@ -523,27 +504,16 @@ def deferred_fit_transform_sklearn_packed(
     name_infix="transformed_packed",
     cache=None,
 ):
-    """
-    Generic wrapper for ANY sklearn transformer using packed format.
-
-    Uses Array[Struct{key, value}] as output type, which allows fully deferred
-    execution without needing to know the output schema at graph construction time.
-    Feature names are resolved at execution time via get_feature_names_out().
-    """
-
     @toolz.curry
-    def fit(df, *args, cls, params_pickled):
-        params = cloudpickle.loads(params_pickled)
+    def fit(df, *args, cls, params):
         instance = cls(**dict(params))
         instance.fit(df, *args)
         return instance
 
-    params_pickled = cloudpickle.dumps(params)
-
     return deferred_fit_transform(
         expr=expr,
         features=list(features),
-        fit=fit(cls=cls, params_pickled=params_pickled),
+        fit=fit(cls=cls, params=params),
         other=kv_encode_output,
         return_type=KV_ENCODED_TYPE,
         target=target,
