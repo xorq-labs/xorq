678e33e31b7e0db28d04aa7af84651f7418f7257
dlovell <dlovell@gmail.com>
2026-02-04 16:38:36 -0500

    ref

--- python/xorq/expr/ml/pipeline_lib.py
+++ python/xorq/expr/ml/pipeline_lib.py
@@ -403,22 +403,24 @@ class FittedStep:
     @property
     @functools.cache
     def _deferred_fit_transform(self):
-        if not self.is_transform:
-            return None
+        assert self.is_transform
         return DeferredFitOther.from_fitted_step(self, mode="transform")
 
     @property
     @functools.cache
     def _deferred_fit_predict(self):
-        if not self.is_predict:
-            return None
+        assert self.is_predict
         return DeferredFitOther.from_fitted_step(self, mode="predict")
 
     @property
     @functools.cache
     def _deferred_fit_other(self):
         # Backward compat: prefer transform, fall back to predict
-        return self._deferred_fit_transform or self._deferred_fit_predict
+        return (
+            self._deferred_fit_transform
+            if self.is_transform
+            else self._deferred_fit_predict
+        )
 
     @property
     def deferred_model(self):
