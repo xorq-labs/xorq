c9aeff48593d3055fab53a18da897693b50f8f02
Dan Lovell <dlovell@gmail.com>
2026-02-12 17:23:31 -0500

    ref(api): add xorq.ibis_yaml.{build_expr,load_expr} (#1574)

--- python/xorq/api.py
+++ python/xorq/api.py
@@ -11,7 +11,6 @@ from xorq import config
 from xorq import expr
 from xorq.config import options
 
-
 from xorq.expr import api
 from xorq.expr.api import *  # noqa: F403
 from xorq import ml
@@ -20,6 +19,10 @@ from xorq.backends.xorq import Backend
 from xorq.internal import SessionConfig
 
 from xorq.loader import load_backend
+from xorq.ibis_yaml.compiler import (
+    build_expr,
+    load_expr,
+)
 
 # Import catalog API - both namespace and convenience functions
 from xorq import catalog_api as catalog
@@ -41,6 +44,8 @@ __all__ = [  # noqa: PLE0604
     "catalog",
     "read_catalog",
     "read_build",
+    "build_expr",
+    "load_expr",
     *api.__all__,
     *ml.__all__,
     *udf.__all__,
