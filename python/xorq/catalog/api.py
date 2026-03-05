d3a6bd9c0b7ea69494c326dcb6cb90e6844a706b
Daniel Mesejo <mesejoleon@gmail.com>
2026-03-05 15:48:26 +0100

    ref: remove unused imports

--- python/xorq/catalog/api.py
+++ python/xorq/catalog/api.py
@@ -9,8 +9,6 @@ from xorq.catalog.expr_utils import (
     build_expr_context_tgz,
     load_expr_from_tgz,
 )
-from xorq.common.utils.graph_utils import has_unbound_table
-from xorq.ibis_yaml.compiler import ExprKind
 
 
 __all__ = [  # noqa: PLE0604
@@ -18,9 +16,7 @@ __all__ = [  # noqa: PLE0604
     "CatalogAddition",
     "CatalogEntry",
     "CatalogRemoval",
-    "ExprKind",
     "build_expr_context",
     "build_expr_context_tgz",
-    "has_unbound_table",
     "load_expr_from_tgz",
 ]
